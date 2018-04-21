2606c7dd2bec967bcbd0617d5e2ee05de3436bbc
verb55nta <verb55nta@gmail.com>
2018-04-21 23:43:37 +0900

    accepted x-ray

--- 300/x-ray/source/main.cpp
+++ 300/x-ray/source/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 int main(void){
 
   int n;
@@ -12,17 +13,17 @@ int main(void){
 
   int item[26][5]; //A~Z //min_x,min_y,max_x,max_y
   //char layer[7];
-  //vector<list<int>> layer(7);
+  //vector<list<int>> obj(26);
   int layer[26][26];
 
   cin>>n;
-  cout << n << endl;
+  //cout << n << endl;
 
   for(int i=0;i<n;i++){
     cin>>h;
-    cout << "h:"<<h << endl;
+    //cout << "h:"<<h << endl;
     cin>>w;
-    cout << "w:"<<w << endl;
+    //cout << "w:"<<w << endl;
 
     for(int j=0;j<50;j++){
       for(int k=0;k<50;k++){
@@ -35,7 +36,7 @@ int main(void){
     }
 
     for(int j=0;j<26;j++){
-      for(int k=0;k<26;j++){
+      for(int k=0;k<26;k++){
         layer[j][k]=0;
       }
     }
@@ -49,11 +50,11 @@ int main(void){
 
     for(int j=0;j<h;j++){
       for(int k=0;k<w;k++){
-        cout<<screen[j][k];
+        //cout<<screen[j][k];
       }
-      cout << endl;
+      //cout << endl;
     }
-    cout << endl;
+    //cout << endl;
 
     for(int j=0;j<h;j++){
       for(int k=0;k<w;k++){
@@ -77,9 +78,9 @@ int main(void){
 
     for(int j=0;j<26;j++){
       if(item[j][0]==1){
-        cout<<(char)(j+'A')<<":"<<endl;
-        cout<<"(min_x,min_y)=(" <<item[j][1] << "," <<item[j][2]  << ")" << endl;
-        cout<<"(max_x,max_y)=(" <<item[j][3] << "," <<item[j][4]  << ")" << endl;
+        //cout<<(char)(j+'A')<<":"<<endl;
+        //cout<<"(min_x,min_y)=(" <<item[j][1] << "," <<item[j][2]  << ")" << endl;
+        //cout<<"(max_x,max_y)=(" <<item[j][3] << "," <<item[j][4]  << ")" << endl;
 
         int min_x=item[j][1];
         int min_y=item[j][2];
@@ -87,18 +88,77 @@ int main(void){
         int max_y=item[j][4];
 
 
-        for(int cnt1=min_y;cnt1<max_y;cnt1++){
-          for(int cnt2=min_x;cnt2<max_x;cnt2++){
-            //if(screen[cnt1][cnt2] != j+'A' ){
-            //  cout << j+'A';
-            //}
+        for(int cnt1=min_y;cnt1<=max_y;cnt1++){
+          for(int cnt2=min_x;cnt2<=max_x;cnt2++){
+            //cout << cnt2 << "," << cnt1 << endl;
+            if(screen[cnt1][cnt2] != j+'A' ){
+              //cout << (char)(j+'A');
+              //cout << screen[cnt1][cnt2];
+
+              if(screen[cnt1][cnt2] >= 'A' && screen[cnt1][cnt2] <= 'Z'){
+
+                int upper_c = screen[cnt1][cnt2]-'A';
+                //cout << (char) (upper_c+'A') << endl;
+                //cout << j << endl;
+                if(layer[j][upper_c] == -1){
+                  //cout << "SUSPICIOUS1" << endl;
+                  goto SUS;
+                }
+                else{
+                  if(layer[j][upper_c] == -1){
+                    //cout << "SUSPICIOUS2" << endl;
+                    goto SUS;
+                  }
+                  else{
+                    layer[j][upper_c] = 1;
+                    //cout << "added!" << endl;
+                  }
+                  for(int cnt3=0;cnt3<26;cnt3++){
+                    if(layer[upper_c][cnt3]==1){
+                      layer[j][cnt3]=layer[upper_c][cnt3];
+                    }
+                  }
+                }
+                for(int cnt3=0;cnt3<26;cnt3++){
+                  for(int cnt4=0;cnt4<26;cnt4++){
+                    if(layer[cnt3][cnt4]*layer[cnt4][cnt3] == 1 ){
+                      //cout << "SUSPICIOUS3" << endl;
+                      goto SUS;
+                    }
+                    else{
+                      if(layer[cnt4][cnt3]!=0){
+                        layer[cnt3][cnt4] = layer[cnt4][cnt3]*-1;
+                      }
+                    }
+                  }
+                }
+
+                for(int cnt3=0;cnt3<26;cnt3++){
+                  for(int cnt4=0;cnt4<26;cnt4++){
+                      //cout << layer[cnt3][cnt4] << " ";
+                  }
+                  //cout << endl;
+                }
+
+              }
+
+              else{
+                //cout << "SUSPICIOUS4" << endl;
+                goto SUS;
+                break;
+              }
+            }
           }
         }
-        cout << endl;
+        //cout << endl;
       }
     }
-
-
+    //cout << "This is Safe" << endl;
+    cout << "SAFE" << endl;
+    continue;
+    SUS:
+      //cout << "This is SUSPICIOUS!" << endl;
+      cout << "SUSPICIOUS" << endl;
 
   }
   return 0;
